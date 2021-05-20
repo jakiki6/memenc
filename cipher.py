c9ae3aeceda1f29b5135094c73c60c7615481e6d
Jakob Kirsch <jakob.kirsch@web.de>
2021-05-20 21:36:52 +0200

    Fixes

--- cipher.py
+++ cipher.py
@@ -21,17 +21,19 @@ class AESCipher(object):
         return self._unpad(cipher.decrypt(enc[AES.block_size:]))
 
     def _pad(self, s):
-        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs).encode()
+        while len(s) % self.bs:
+            s += bytes([random.randint(0, 255)])
+        return s
 
-    @staticmethod
-    def _unpad(s):
-        return s[:-ord(s[len(s)-1:])]
+    def _unpad(self, s):
+        while len(s) % self.bs:
+            s = s[:-1]
+        return s
 
 def encrypt(data: bytes):
     cipher = AESCipher(bytes(14) + bytes([random.randint(0, 255), random.randint(0, 255)]))
-    data = b"\x13\x37\x69\x69" + data
 
-    data = bytearray(b"\x69" + cipher.encrypt(data))
+    data = bytearray(b"\x69" + cipher.encrypt(bytes(16)) + cipher.encrypt(data))
 
     xor = random.randint(0, 256)
     for i in range(0, len(data)):
@@ -41,18 +43,18 @@ def encrypt(data: bytes):
 
 def decrypt(data: bytes):
     for xor in range(0, 256):
-        _data = bytearray(data)
-        for i in range(0, len(_data)):
-            _data[i] ^= xor
-        if _data[0] == 0x69:
+        if data[0] ^ xor == 0x69:
             print(f"found xor 0x{hex(xor)[2:].zfill(2)}")
-            data = bytes(_data[1:])
+            data = bytearray(data[1:])
+            for i in range(0, len(data)):
+                data[i] ^= xor
             break
 
     for key in range(0, 65536):
         cipher = AESCipher(bytes(14) + bytes([key >> 8, key & 0xff]))
-        _data = cipher.decrypt(data)
-        if _data.startswith(b"\x13\x37\x69\x69"):
+        if cipher.decrypt(data[:32]) == bytes(16):
             print(f"found key 0x{hex(key)[2:].zfill(4)}")
-            return _data[4:]
+            data = cipher.decrypt(data[16:])
+            return data
+            
     raise ValueError("key not found :(")
